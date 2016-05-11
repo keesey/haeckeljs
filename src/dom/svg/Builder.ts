42a0a78173ee7dd964cb5898500b4fb2993504b1
Mike Keesey <keesey@gmail.com>
2016-05-11 09:43:50 -0700

    Update Builder.ts

---
+++ src/dom/svg/Builder.ts
@@ -0,0 +1,16 @@
+import {Builder as DOMBuilder} from '../Builder';
+import {NAMESPACE} from './NAMESPACE';
+
+export class Builder extends DOMBuilder
+{
+	constructor(document: Document, element: Element);
+	constructor(document: Document, localName: string);
+	constructor(document: Document, a: Element | string)
+	{
+		super(document, typeof a === 'string' ? NAMESPACE : element, typeof a === 'string' ? a : undefined);
+		if (this.uri !== NAMESPACE)
+		{
+			throw new ArgumentError('Not a valid SVG element.');
+		}
+	}
+}
