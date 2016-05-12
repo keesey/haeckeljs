9ad482fa4413d1c11e77cec0b248134f74e6bf46
Mike Keesey <keesey@gmail.com>
2016-05-12 11:30:22 -0700

    Code for `systematics/characters/matrices/EMPTY`.

---
+++ src/systematics/character/matrices/EMPTY.ts
@@ -0,0 +1,13 @@
+import {Character} from '../Character';
+import {EMPTY as EMPTY_LIST} from '../../../lists/EMPTY';
+import {EMPTY as EMPTY_SET} from '../../../sets/EMPTY';
+import {Matrix} from './Matrix';
+import {Set} from '../../../sets/Set';
+import {Taxic} from '../taxonomy/Taxic';
+
+export const EMPTY = Object.freeze<Matrix<Set>>({
+	characters: EMPTY_SET,
+	characterList: EMPTY_LIST,
+	hashMap: Object.freeze({}),
+	taxon: EMPTY_SET
+});
