220d67b23a4e9d5270487c2fbb651b66e88f2f3f
Mike Keesey <keesey@gmail.com>
2016-05-12 12:45:34 -0700

    Update includes.ts

---
+++ src/lists/includes.ts
@@ -0,0 +1,20 @@
+import {equal} from '../equal';
+
+export function includes<T>(a: T[], b: T[]): boolean
+{
+	const an = a.length;
+	const bn = b.length;
+	if (bn >= an)
+	{
+		return false;
+	}
+	const n = an - bn;
+	for (let i = 0; i <= n; ++i)
+	{
+		if (equal(b, a.slice(i, bn)))
+		{
+			return true;
+		}
+	}
+	return false;
+}
