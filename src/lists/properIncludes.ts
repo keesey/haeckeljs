d3ceeb6fd639ff230c409aea9143338e506d4799
Mike Keesey <keesey@gmail.com>
2016-05-12 12:45:56 -0700

    Update properIncludes.ts

--- src/lists/properIncludes.ts
+++ src/lists/properIncludes.ts
@@ -1,6 +1,6 @@
 import {equal} from '../equal';
 
-export function includes<T>(a: T[], b: T[]): boolean
+export function properIncludes<T>(a: T[], b: T[]): boolean
 {
 	const an = a.length;
 	const bn = b.length;
