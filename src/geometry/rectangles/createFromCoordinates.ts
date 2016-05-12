17098119b50edfcbdee14f2e5f60dda2cd174d90
Mike Keesey <keesey@gmail.com>
2016-05-12 13:14:01 -0700

    Update createFromCoordinates.ts

--- src/geometry/rectangles/createFromCoordinates.ts
+++ src/geometry/rectangles/createFromCoordinates.ts
@@ -1,7 +1,22 @@
 import {Rectangle} from './Rectangle';
 import {create} from './create';
 
-export function createFromCoordinaess(x: number, y: number, x2: number, y2: number): Rectangle
+export function createFromCoordinates(x: number, y: number, x2: number, y2: number): Rectangle
 {
-	return create(x, y, x2 - x, y2 - y);
+	if (x <= x2)
+	{
+		if (y <= y2)
+		{
+			return create(x, y, x2 - x, y2 - y);
+		}
+		else
+		{
+			return create(x, y2, x2 - x, y - y2);
+		}
+	}
+	else if (y <= y2)
+	{
+		return create(x2, y, x - x2, y2 - y);
+	}
+	return create(x2, y2, x - x2, y - y2);
 }
