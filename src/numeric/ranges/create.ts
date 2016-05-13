d16ad93cf3c3a20fbe5da1feb871ff8fa1c47328
Mike Keesey <keesey@gmail.com>
2016-05-13 14:24:12 -0700

    Update create.ts

---
+++ src/numeric/ranges/create.ts
@@ -0,0 +1,68 @@
+import {EMPTY} from '../sets/EMPTY';
+import {PRECISION} from '../numeric/PRECISION';
+import {Range} from './Range';
+import {NONNEGATIVES} from './NONNEGATIVES';
+import {NONPOSITIVES} from './NONPOSITIVES';
+import {ONE} from './ONE';
+import {REALS} from './REALS';
+import {ZERO} from './ZERO';
+import {ZERO_TO_ONE} from './ZERO_TO_ONE';
+
+export function create(min: number, max: number = NaN): Range
+{
+	if (isNaN(min))
+	{
+		return EMPTY;
+	}
+	min = Math.round(min * PRECISION) / PRECISION;
+	if (max === Infinity)
+	{
+		if (min === 0)
+		{
+			return NONNEGATIVES;
+		}
+		if (min === -Infinity)
+		{
+			return REALS;
+		}
+	}
+	else if (min === -Infinity && max === 0)
+	{
+		return NONPOSITIVES;
+	}
+	if (isNaN(max))
+	{
+		max = min;
+	}
+	else
+	{
+		max = Math.round(max * PRECISION) / PRECISION;
+	}
+	if (!(min <= max))
+	{
+		throw new Error(min + ' is not less than or equal to ' + max + '.');
+	}
+	if (min === 0)
+	{
+		if (max === 1)
+		{
+			return ZERO_TO_ONE;
+		}
+		if (max === 0)
+		{
+			return ZERO;
+		}
+	}
+	else if (min === 1 && max === 1)
+	{
+		return ONE;
+	}
+	return Object.freeze({
+		empty: false,
+		hash: '[' + min + '…' + max + ']',
+		max: max,
+		mean: (max + min) / 2,
+		min: min,
+		size: max - min
+	});
+}
