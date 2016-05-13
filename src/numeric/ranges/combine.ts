b1b5642276eaebf744625b34197d18e32ac3b7d8
Mike Keesey <keesey@gmail.com>
2016-05-13 14:27:08 -0700

    Update combine.ts

---
+++ src/numeric/ranges/combine.ts
@@ -0,0 +1,38 @@
+import {Range} from './Range';
+import {create} from './create';
+
+export function combine(ranges: Range[]): Range
+{
+	const n = ranges.length;
+	if (n === 1)
+	{
+		return ranges[0];
+	}
+	if (n === 0)
+	{
+		return null;
+	}
+	let min = NaN;
+	let max = NaN;
+	for (let i = 0; i < n; ++i)
+	{
+		const range = ranges[i];
+		if (!range)
+		{
+			continue;
+		}
+		if (isNaN(min) || range.min < min)
+		{
+			min = range.min;
+		}
+		if (isNaN(max) || range.max > max)
+		{
+			max = range.max;
+		}
+	}
+	if (isNaN(min))
+	{
+		return null;
+	}
+	return create(min, max);
+}
