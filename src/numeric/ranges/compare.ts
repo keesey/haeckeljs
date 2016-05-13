360ef20c082575f99f62cf2f960f2257995221c7
Mike Keesey <keesey@gmail.com>
2016-05-13 14:28:49 -0700

    Update compare.ts

---
+++ src/numeric/ranges/compare.ts
@@ -0,0 +1,14 @@
+import {Range} from './Range';
+
+export default function compare(a: Range, b: Range): number
+{
+	if (a.empty)
+	{
+		return b.empty ? 0 : -1;
+	}
+	if (b.empty)
+	{
+		return 1;
+	}
+	return a.mean - b.mean;
+}
